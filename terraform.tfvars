884740f36fe593b91d00f2f4b8886f0454a29b0c
Jude <gangdia@yahoo.com>
2023-06-28 10:32:06 -0600

    terraform

---
+++ terraform.tfvars
@@ -0,0 +1,6 @@
+my_region     = "us-east-2"
+profile       = "default"
+topic_name    = "sns-dev"
+endpoint_type = "gangdia@yahoo.com"
+protocol_type = "email"
+team          = "dev"
---
+++ variables.tf
@@ -0,0 +1,25 @@
+variable "my_region" {
+  type    = string
+  default = "us-east-1"
+}
+
+variable "profile" {
+  type    = string
+  default = "default"
+}
+
+variable "topic_name" {
+  default = "sns-dev"
+}
+
+variable "team" {
+  default = "dev"
+}
+
+variable "protocol_type" {
+  default = "email"
+}
+
+variable "endpoint_type" {
+  default = "dfsee@gmail.com"
+}
